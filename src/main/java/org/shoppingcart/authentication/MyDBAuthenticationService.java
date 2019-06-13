bb875910d957e6eac5574fc4657d33e8af0db13b
KrishnaGehlot23 <krishnagehlotr@gmail.com>
2019-06-13 19:06:30 +0530

    Added authentication class

---
+++ src/main/java/org/shoppingcart/authentication/MyDBAuthenticationService.java
@@ -0,0 +1,45 @@
+package org.shoppingcart.authentication;
+
+import java.util.ArrayList;
+
+import org.shoppingcart.dao.AccountDAO;
+import org.shoppingcart.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyDBAuthenticationService implements UserDetailsService {
+
+	@Autowired
+	private AccountDAO accountDAO;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Account account = accountDAO.findAccount(username);
+
+		if (account == null) {
+			throw new UsernameNotFoundException("User" + username + "was not found in our records");
+		}
+
+		// EMPLOYEE,MANAGER,..
+		String role = account.getUserRole();
+		ArrayList<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+
+		// ROLE_EMPLOYEE, ROLE_MANAGER
+		SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
+		grantList.add(authority);
+
+		boolean enabled = account.isActive();
+		boolean accountNonExpired = true;
+		boolean credentialsNonExpired = true;
+		boolean accountNonLocked = true;
+
+		UserDetails userDetails = (UserDetails) new User(account.getUserName(), account.getPassword(), enabled,
+				accountNonExpired, credentialsNonExpired, accountNonLocked, grantList);
+		return userDetails;
+	}
+}
