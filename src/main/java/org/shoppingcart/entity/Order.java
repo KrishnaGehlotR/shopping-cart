9be7221b6918ef8069a44726ba692c5ce63391dc
KrishnaGehlot23 <krishnagehlotr@gmail.com>
2019-06-13 12:10:35 +0530

    Added data member of entity class order

--- src/main/java/org/shoppingcart/entity/Order.java
+++ src/main/java/org/shoppingcart/entity/Order.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -15,14 +17,140 @@ public class Order implements Serializable {
 	private static final long serialVersionUID = 8425514605540316080L;
 
 	@Id
-	@Column(name="orderId",length=50)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "orderId", length = 50)
 	private String orderId;
-	private Date OrderDate;
+
+	@Column(name = "orderDate", nullable = false)
+	private Date orderDate;
+
+	@Column(name = "orderNum", nullable = false)
 	private int orderNum;
+
+	@Column(name = "amount", nullable = false)
 	private double amount;
 
+	@Column(name = "customerName", length = 255, nullable = false)
 	private String customerName;
+
+	@Column(name = "customerAddress", length = 255, nullable = false)
 	private String customerAddress;
+
+	@Column(name = "customerEmail", length = 255, nullable = false)
 	private String customerEmail;
+
+	@Column(name = "customerPhone", length = 128, nullable = false)
 	private String customerPhone;
-}	
+
+	/**
+	 * @return the orderId
+	 */
+	public String getOrderId() {
+		return orderId;
+	}
+
+	/**
+	 * @param orderId the orderId to set
+	 */
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	/**
+	 * @return the orderDate
+	 */
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	/**
+	 * @param orderDate the orderDate to set
+	 */
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	/**
+	 * @return the orderNum
+	 */
+	public int getOrderNum() {
+		return orderNum;
+	}
+
+	/**
+	 * @param orderNum the orderNum to set
+	 */
+	public void setOrderNum(int orderNum) {
+		this.orderNum = orderNum;
+	}
+
+	/**
+	 * @return the amount
+	 */
+	public double getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	/**
+	 * @return the customerName
+	 */
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	/**
+	 * @param customerName the customerName to set
+	 */
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	/**
+	 * @return the customerAddress
+	 */
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+
+	/**
+	 * @param customerAddress the customerAddress to set
+	 */
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+
+	/**
+	 * @return the customerEmail
+	 */
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+	/**
+	 * @param customerEmail the customerEmail to set
+	 */
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+
+	/**
+	 * @return the customerPhone
+	 */
+	public String getCustomerPhone() {
+		return customerPhone;
+	}
+
+	/**
+	 * @param customerPhone the customerPhone to set
+	 */
+	public void setCustomerPhone(String customerPhone) {
+		this.customerPhone = customerPhone;
+	}
+}
