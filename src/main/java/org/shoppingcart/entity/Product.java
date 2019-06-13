b7c5eb1592eb6351332a0559ddee42b67a438fa7
KrishnaGehlot23 <krishnagehlotr@gmail.com>
2019-06-13 12:25:42 +0530

    Added data member of entity class product

--- src/main/java/org/shoppingcart/entity/Product.java
+++ src/main/java/org/shoppingcart/entity/Product.java
@@ -1,9 +1,129 @@
 package org.shoppingcart.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "product")
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 3490789768232224348L;
 
+	@Id
+	@Column(name = "productId", length = 20, nullable = false)
+	private String productId;
+
+	@Column(name = "code", length = 20, nullable = false)
+	private String code;
+
+	@Column(name = "name", length = 255, nullable = false)
+	private String name;
+
+	@Column(name = "price", length = 255, nullable = false)
+	private double price;
+
+	@Lob
+	@Column(name = "image", length = Integer.MAX_VALUE, nullable = false)
+	private byte[] image;
+
+	// For Sort.
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "createDate", nullable = false)
+	private Date createDate;
+
+	public Product() {
+
+	}
+
+	/**
+	 * @return the productId
+	 */
+	public String getProductId() {
+		return productId;
+	}
+
+	/**
+	 * @param productId the productId to set
+	 */
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public byte[] getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	/**
+	 * @return the createDate
+	 */
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	/**
+	 * @param createDate the createDate to set
+	 */
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
 }
