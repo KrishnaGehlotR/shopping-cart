2f1d67e590cca1515a62a59acd87be046b0f6672
KrishnaGehlot23 <krishnagehlotr@gmail.com>
2019-06-13 12:11:21 +0530

    Added data member of enttity class of order details

--- src/main/java/org/shoppingcart/entity/OrderDetail.java
+++ src/main/java/org/shoppingcart/entity/OrderDetail.java
@@ -4,7 +4,13 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -14,10 +20,16 @@ public class OrderDetail implements Serializable {
 	private static final long serialVersionUID = -3152259856785022168L;
 
 	@Id
-	@Column(name = "id", length = 50, nullable = false)
-	private String id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "orderDetailId", length = 50, nullable = false)
+	private String orderDetailId;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id", nullable = false, foreignKey = @ForeignKey(name = "order_detail_ord_fk"))
 	private Order order;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", nullable = false, foreignKey = @ForeignKey(name = "order_detail_prod_fk"))
 	private Product product;
 
 	@Column(name = "quantity", nullable = false)
@@ -28,4 +40,88 @@ public class OrderDetail implements Serializable {
 
 	@Column(name = "amount", nullable = false)
 	private double amount;
+
+	/**
+	 * @return the orderDetailId
+	 */
+	public String getOrderDetailId() {
+		return orderDetailId;
+	}
+
+	/**
+	 * @param orderDetailId the orderDetailId to set
+	 */
+	public void setOrderDetailId(String orderDetailId) {
+		this.orderDetailId = orderDetailId;
+	}
+
+	/**
+	 * @return the order
+	 */
+	public Order getOrder() {
+		return order;
+	}
+
+	/**
+	 * @param order the order to set
+	 */
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	/**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return product;
+	}
+
+	/**
+	 * @param product the product to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the amount
+	 */
+	public double getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 }
